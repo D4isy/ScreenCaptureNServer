93c8ffdfa4e22ad4170f19ce6cdaa5651f83e6cc
Hapu <junog115@gmail.com>
2017-05-22 13:53:43 +0900

    Update projectServer.c
    Add ThreadQueue, DeleteQueue, Mutex

--- github/OpensourceProject/OpensourceProject/projectServer.c
+++ github/OpensourceProject/OpensourceProject/projectServer.c
@@ -1,9 +1,57 @@
 #include "socket_lib-master\socket_lib.h"
-
+// #include <Shlwapi.h>
 #include <time.h>
+#include <errno.h>
+#include <io.h> // access
+#include <process.h> // _beginthreadex, mutex
 #include <tchar.h>
 
+// #pragma comment(lib, "Shlwapi.lib")
+
 #define BUF_SIZE 10 //((1025)*(512))
+
+typedef struct _DeleteQueue {
+	char path[MAX_PATH];
+	struct _DeleteQueue *next;
+} DeleteQueue;
+
+HANDLE hMutex;
+HANDLE hThread;
+int run_queue_flag=1, run_queue_count=0;
+DeleteQueue *queue_head;
+
+void fileNameMaker(char *fName);
+void __close(SOCKET sockfd, fd_set *readfds);
+UINT WINAPI ThreadQueue(void *arg);
+
+DWORD WINAPI ThreadQueue(void *arg)
+{
+	DeleteQueue *queue_tmp;
+	DeleteQueue *queue_del;
+	while (run_queue_flag) {
+		if (!run_queue_count) {
+			SuspendThread(hThread);
+		}
+
+		WaitForSingleObject(hMutex, INFINITE);
+		queue_tmp = queue_head->next;
+		while (queue_tmp != NULL) {
+			if (remove(queue_tmp->path) != 0) {
+				continue;
+			}
+			else {
+				run_queue_count--;
+				queue_del = queue_tmp;
+				queue_tmp = queue_tmp->next;
+				queue_head->next = queue_tmp;
+				free(queue_del);
+			}
+		}
+		ReleaseMutex(hMutex);
+	}
+	_endthreadex(0);
+	return 0;
+}
 
 void __close(SOCKET sockfd, fd_set *readfds)
 {
@@ -30,6 +78,23 @@ int main(int argc, char **argv)
 	SOCKADDR_IN server_addr, client_addr;
 	int addrlen;
 
+	DWORD dwThreadID;
+
+	hMutex = CreateMutex(
+				NULL,	// 디폴트 보안 관리자
+				FALSE,	// 누구나 소유할 수 있는 상태로 생성
+				NULL	// numaned mutex
+			);
+
+	if (hMutex == NULL) {
+		_tprintf(TEXT("CreateMutex error: %d\n"), GetLastError());
+		return 0;
+	}
+	queue_head = (DeleteQueue *)malloc(sizeof(DeleteQueue));
+	queue_head->path[0] = 0x00;
+	queue_head->next = 0;
+	hThread = (HANDLE)_beginthreadex(NULL, 0, ThreadQueue, NULL, 0, &dwThreadID);
+	
 	FD_ZERO(&readfds);
 	hServSock = creat_server_socket(ip, port);
 
@@ -91,9 +156,23 @@ int main(int argc, char **argv)
 					}
 					else if (readn == SOCKET_ERROR)
 					{
+						DeleteQueue *queueTmp = (DeleteQueue*)malloc(sizeof(DeleteQueue));
 						printf("[fd:%u] file receive error!\n", hClntSock);
 						fclose(fp);
 						__close(hClntSock, &readfds);
+						if (queueTmp == NULL) {
+							printf("[fd: %u] file memory allocation error!\n", hClntSock, fName);
+						}
+						else {
+							WaitForSingleObject(hMutex, INFINITE);
+							strncpy_s(queueTmp->path, MAX_PATH, fName, MAX_PATH);
+							queueTmp->next = NULL;
+							queue_head->next = queueTmp;
+							run_queue_count++;
+							ReleaseMutex(hMutex);
+							puts("thread resume!");
+							ResumeThread(hThread);
+						}
 						break;
 					}
 					fwrite(buf, sizeof(char), BUF_SIZE - 1, fp);
@@ -106,38 +185,42 @@ int main(int argc, char **argv)
 		}
 	}
 
+	run_queue_flag = 0;
+	WaitForSingleObject(hThread, INFINITE);
 	close_client_socket(hServSock);
+	CloseHandle(hThread);
+	free(queue_head);
 	return 0;
 }
 
 void fileNameMaker(char *fName) {
-   time_t timer;
-   struct tm *curTime;
+	time_t timer;
+	struct tm *curTime;
 
-   char strFolderPath[] = "C:\\server";
-   int nResult, idx=0;
+	char strFolderPath[] = "C:\\server";
+	int nResult, idx=0;
 
-   timer = time(NULL);
-   curTime = localtime(&timer);
-	
-   nResult = mkdir(strFolderPath);
-	
-   sprintf(fName, "%s\\%04d%02d%02d_%02d%02d%02d.png", strFolderPath,
-      curTime->tm_year + 1900, curTime->tm_mon + 1, curTime->tm_mday,
-      curTime->tm_hour, curTime->tm_min, curTime->tm_sec
-   );
+	timer = time(NULL);
+	curTime = localtime(&timer);
 
-   while (1) {
-      //if (PathFileExists(fName) == TRUE) {
-      //   break;
-      //}
-      if (access(fName, 0) == -1) {
-         break;
-      }
+	nResult = mkdir(strFolderPath);
 
-      sprintf(fName, "%s\\%04d%02d%02d_%02d%02d%02d(%d).png", strFolderPath,
-         curTime->tm_year + 1900, curTime->tm_mon + 1, curTime->tm_mday,
-         curTime->tm_hour, curTime->tm_min, curTime->tm_sec, idx++
-         );
-   }
+	sprintf(fName, "%s\\%04d%02d%02d_%02d%02d%02d.png", strFolderPath,
+		curTime->tm_year + 1900, curTime->tm_mon + 1, curTime->tm_mday,
+		curTime->tm_hour, curTime->tm_min, curTime->tm_sec
+	);
+
+	while (1) {
+		//if (PathFileExists(fName) == TRUE) {
+		//	break;
+		//}
+		if (access(fName, 0) == -1) {
+			break;
+		}
+
+		sprintf(fName, "%s\\%04d%02d%02d_%02d%02d%02d(%d).png", strFolderPath,
+			curTime->tm_year + 1900, curTime->tm_mon + 1, curTime->tm_mday,
+			curTime->tm_hour, curTime->tm_min, curTime->tm_sec, idx++
+			);
+	}
 }
